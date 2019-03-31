4d5a3c619581e86a7d8e9c917b47e7b3adb54fb4
tendanger <tendanger@outlook.com>
2019-03-31 22:35:41 +0800

    change random2

--- Simulation_Demo.py
+++ Simulation_Demo.py
@@ -1,18 +1,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 S0 = 0 #初始值
-I = 20 #仿真10次
-T = 10 #时间
-S = np.zeros((T + 1, I))
+I = 2 #仿真次数
+T = 1 #时间周期
+S = np.zeros((T + 1, I)) # T+1行 I列 矩阵
+#输出矩阵形状
+print(S.shape)
+#矩阵初始值为0
 S[0] = S0
-for t in range(1, T + 1):
-    #均匀分布（-10，10）
-    S[t] = S[t - 1] + np.random.uniform(-10,10,I)
-    #输出矩阵
-    print(S[t])
 
+#给矩阵赋值，每列为一个企业生命周期
+for t in range(1, T + 1):
+    #均匀分布（-10，10） 上期 + 本期的均匀分布值
+    #每行为同一周期所有企业的上期绩效 + 本期绩效
+    S[t] = S[t - 1] + np.random.uniform(-10,10,I)
+#输出矩阵
+print(S)
+
+#作图，按列
 plt.plot(S[:, :], lw=1.5)
 plt.xlabel('time')
 plt.ylabel('price')
